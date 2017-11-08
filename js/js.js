b7566cbe83d12690545ff893646b69281160e45d
WinterSunnyJun <496900910@qq.com>
2017-11-08 22:21:12 +0800

    ajun:

--- js/js.js
+++ js/js.js
@@ -1,7 +1,3 @@
-/**
- * Created by DELL on 2017/11/7.
- */
-
 $(function () {
     //banner
     $('.banner-list').bxSlider({
@@ -10,5 +6,19 @@ $(function () {
     $('.banner .bx-prev').html('');
     $('.banner .bx-next').html('');
     $('.banner .bx-pager-link').html('');
-    $('.banner .bx-pager').css('margin-left',-($('.bx-pager').width())/2 + 'px')
+    $('.banner .bx-pager').css('margin-left',-($('.bx-pager').width())/2 + 'px');
+
+    $('.index-news-title').hover(function () {
+        $('.index-news-hide').show();
+    })
+
+    $('.index-news-hide').mouseleave(function () {
+        $(this).hide();
+    })
+
+    $('.news-hide-btn li').click(function () {
+        var Now = $(this).index();
+        $('.news-hide-btn li').eq(Now).addClass('active').siblings().removeClass('active');
+        $('.index-news-hide dd > .news-hide-item').eq(Now).show().siblings().hide();
+    })
 })
