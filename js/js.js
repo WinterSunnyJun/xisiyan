2a3cd75fd337613e446a7ad872a7a287db0af04b
WinterSunnyJun <496900910@qq.com>
2017-11-10 19:19:33 +0800

    ajun

--- js/js.js
+++ js/js.js
@@ -21,4 +21,60 @@ $(function () {
         $('.news-hide-btn li').eq(Now).addClass('active').siblings().removeClass('active');
         $('.index-news-hide dd > .news-hide-item').eq(Now).show().siblings().hide();
     })
+    //招商加盟
+    $('#jms-show').bxSlider({
+        auto:true,
+        slideMargin:30,
+        minSlides:5,
+        maxSlides:5,
+        moveSlides:5,
+        slideWidth:216,
+        autoHover:true
+
+    });
+    $('.jms-show-wrapper .bx-pager-link').html('');
+    $('.jms-show-wrapper .bx-pager').css('margin-left',-$('.bx-pager').width()/2);
+
+//    产品详情
+    $('.cpxq-pic-btn ol li').hover(function () {
+        var Now = $(this).index();
+        $('.cpxq-pic-btn ol li').eq(Now).addClass('active').siblings().removeClass('active');
+        $('.cpxq-pic-list li').eq(Now).show().siblings().hide();
+    })
+
+    // var moveRange = $('.cpxq-pic-btn ol li').outerWidth(true)+ 'px';
+    // var liSum = $('.cpxq-pic-btn ol li').length;
+    // var cpCount = 0;
+    // function getPost() {
+    //     var l = 0;
+    //     var t = 0;
+    //
+    // }
+    // $('#cpL').click(function () {
+        // cpCount++;
+        // if(cpCount > liSum){
+        //     cpCount = 0;
+        // }
+
+        // $('.cpxq-pic-btn ol').animate({left: - moveRange + 'px'});
+    // })
+    // $('#cpR').click(function () {
+        // cpCount++;
+        // if(cpCount > liSum){
+        //     cpCount = 0;
+        // }
+
+        // $('.cpxq-pic-btn ol').animate({left:  moveRange + 'px'});
+    // })
+
+    $('.cpxq-pic-btnbox ol').bxSlider({
+        slideMargin:36,
+        minSlides:4,
+        maxSlides:4,
+        moveSlides:1,
+        slideWidth:108
+    })
+    $('.cpxq-pic-btnbox .bx-prev').html('');
+    $('.cpxq-pic-btnbox .bx-next').html('');
+
 })
